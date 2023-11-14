32e47a65b790d78145b1539be87f4ab06ffbad5d
Jhonjaxde <jjarteaga@uce.edu.ec>
2023-11-13 19:40:50 -0500

    taller5

--- src/main/java/com/uce/edu/Pa2U1P5JaApplication.java
+++ src/main/java/com/uce/edu/Pa2U1P5JaApplication.java
@@ -1,24 +1,38 @@
 package com.uce.edu;
 
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.uce.edu.ioc.di.Estudiante;
+
 @SpringBootApplication
 public class Pa2U1P5JaApplication implements CommandLineRunner{
-
+	// Para hacer inyeccion de dependencias 
+	// declaramos un atributo de la clase estudiante 
+	// usamos una anotaciones de nivel de atributo 
+	@Autowired
+	private Estudiante estudiante;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U1P5JaApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		//TODO Auto-generated method stub
-		System.out.println("hola "+"mundo");
-		System.out.println("Soy"+" "+"Jhon"+" "+"Arteaga");
-		System.out.println("cambio en la misma rama");
-		System.out.println("cambio en nueva rama taller 3");
-		System.out.println("taller4 version 2");
+	
+	this.estudiante.setNombre("Jhon");	
+	this.estudiante.setApellido("Arteaga");
+	this.estudiante.setCedula("1708923873");
+	this.estudiante.setSalario(new BigDecimal(100));
+	// AQUI SE DESTRUYE porque ya no se lo utiliza pero se 
+	//encarga el garbage collector
+	System.out.println(estudiante);
+		
+		
 	}
 
 }
