101d8e9944c934285322b449ec744d9f69440d38
Jhonjaxde <jjarteaga@uce.edu.ec>
2023-11-24 19:04:36 -0500

    taller9

---
+++ src/main/java/com/uce/edu/transferencia/repository/CuentaBancariaRepositoryImpl.java
@@ -0,0 +1,51 @@
+package com.uce.edu.transferencia.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.uce.edu.repository.modelo.Materia;
+import com.uce.edu.transferencia.repository.modelo.CuentaBancaria;
+import com.uce.edu.transferencia.repository.modelo.Transferencia;
+
+@Repository
+public class CuentaBancariaRepositoryImpl implements ICuentaBancariaRepository{
+	
+	private static List<CuentaBancaria> base= new ArrayList<>();
+	
+	@Override
+	public CuentaBancaria seleccionar(String numero) {
+		//System.out.println("seleccionamos:"+ numero);
+		for(CuentaBancaria cuentaBancaria:base) {
+			if(cuentaBancaria.getNumero().equals(numero)) {
+				return cuentaBancaria;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public void insertar(CuentaBancaria cuentaBancaria) {
+		base.add(cuentaBancaria);
+		//System.out.println("Se inserto:"+ materia);
+		
+	}
+
+	@Override
+	public void actualizar(CuentaBancaria cuentaBancaria) {
+		this.eliminar(cuentaBancaria.getNumero());
+		this.insertar(cuentaBancaria);
+		//System.out.println("Se actualiza:" + materia);
+		
+	}
+
+	@Override
+	public void eliminar(String numero) {
+		CuentaBancaria cuentaBancaria = this.seleccionar(numero);
+		base.remove(cuentaBancaria);
+		//System.out.println("Se borra:"+ numero);
+		
+	}
+
+}
---
+++ src/main/java/com/uce/edu/transferencia/repository/TransferenciaRepositoryImpl.java
@@ -0,0 +1,49 @@
+package com.uce.edu.transferencia.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+
+import com.uce.edu.transferencia.repository.modelo.Transferencia;
+
+@Repository
+public class TransferenciaRepositoryImpl implements ITransferenciaRepository{
+	
+	private static List<Transferencia> base= new ArrayList<>();
+	@Override
+	public Transferencia seleccionar(String numero) {
+		//System.out.println("seleccionamos:"+ numero);
+		for(Transferencia transferencia:base) {
+			if(transferencia.getNumero().equals(numero)) {
+				return transferencia;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public void insertar(Transferencia transferencia) {
+		base.add(transferencia);
+		//System.out.println("Se inserto:"+ transferencia);
+		
+	}
+
+	@Override
+	public void actualizar(Transferencia transferencia) {
+		this.eliminar(transferencia.getNumero());
+		this.insertar(transferencia);
+		//System.out.println("Se actualiza:" + transferencia);
+		
+	}
+
+	@Override
+	public void eliminar(String numero) {
+		Transferencia transferencia = this.seleccionar(numero);
+		base.remove(transferencia);
+		//System.out.println("Se borra:"+ numero);
+		
+	}
+
+}
